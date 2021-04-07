3b1fdd6f85013747663050effe3999c21f846e47
wangyang <764817302@qq.com>
2021-04-07 16:42:15 +0800

    加载动画修改

--- tbswrap/src/main/java/com/ahcj/tbswrap/x5webview/X5WebViewProgressDialog.java
+++ tbswrap/src/main/java/com/ahcj/tbswrap/x5webview/X5WebViewProgressDialog.java
@@ -4,6 +4,10 @@ package com.ahcj.tbswrap.x5webview;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 import com.ahcj.tbswrap.R;
 
@@ -16,6 +20,7 @@ public class X5WebViewProgressDialog extends AlertDialog {
 
 	/**对话框依赖的窗口*/
 	private Context context;
+	private ImageView img;
 
 	/**
 	 * 使用固定样式*/
@@ -41,6 +46,17 @@ public class X5WebViewProgressDialog extends AlertDialog {
 		setCanceledOnTouchOutside(true);
 		//设置为false，按返回键不能退出
 		setCancelable(true);
+
+		img=findViewById(R.id.img);
+
+		RotateAnimation rotate  = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		LinearInterpolator lin = new LinearInterpolator();
+		rotate.setInterpolator(lin);
+		rotate.setDuration(2000);//设置动画持续周期
+		rotate.setRepeatCount(-1);//设置重复次数
+		rotate.setFillAfter(true);//动画执行完后是否停留在执行完的状态
+		rotate.setStartOffset(10);//执行前的等待时间
+		img.setAnimation(rotate);
 
 	}
 }
